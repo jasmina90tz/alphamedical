8c97f488a87d44a147f1a480eb257da03892e95d
jasmina.zukancic <jasmina.zukancic@fsbtech.com>
2023-06-02 16:21:45 +0200

    Site progress

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StartComponent } from './start/start.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ForCompaniesComponent } from './for-companies/for-companies.component';
+import { ForApplicantsComponent } from './for-applicants/for-applicants.component';
+import { VacanciesComponent } from './vacancies/vacancies.component';
+import { ContactComponent } from './contact/contact.component';
+import { ServicesComponent } from './services/services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'start', component: StartComponent },
+  { path: 'aboutus', component: AboutusComponent },
+  { path: 'for-companies', component: ForCompaniesComponent },
+  { path: 'for-applicants', component: ForApplicantsComponent },
+  { path: 'vacancies', component: VacanciesComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'services', component: ServicesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { StartComponent } from './start/start.component';
+import { ForCompaniesComponent } from './for-companies/for-companies.component';
+import { ForApplicantsComponent } from './for-applicants/for-applicants.component';
+import { VacanciesComponent } from './vacancies/vacancies.component';
+import { ContactComponent } from './contact/contact.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ServicesComponent } from './services/services.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutusComponent,
+    StartComponent,
+    ForCompaniesComponent,
+    ForApplicantsComponent,
+    VacanciesComponent,
+    ContactComponent,
+    NavbarComponent,
+    ServicesComponent,
+    FooterComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
+  exports: [NavbarComponent],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
